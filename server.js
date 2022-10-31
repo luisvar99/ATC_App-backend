4646fddf422941e25ad4052e6be95d48991b588f
luisvar99 <luisvar2703@gmail.com>
2022-10-31 10:10:35 -0400

    first commit

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 4000
-
 app.listen(PORT, (req, res) => {
     console.log(`Listening on port ${PORT}`);
 /* sequelize.authenticate().then(() => {
