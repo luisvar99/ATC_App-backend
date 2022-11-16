227734d435c78334e166af573ca5504eafb5edf5
luisvar99 <luisvar2703@gmail.com>
2022-11-15 22:19:37 -0400

    Avanve

--- Controllers/Reservaciones.js
+++ Controllers/Reservaciones.js
@@ -53,14 +53,16 @@ const DeleteReservacion = async (req, res) => {
 const GetCanchaReservaciones = async (req, res) => {
 
     const idCancha = req.params.idCancha
+    const fecha = req.params.fecha
+     console.log(fecha) 
 
     try {
         const result = await db.query(`SELECT u.id, u.username, h.inicio, h.fin, h.hora_inicio, h.id_horario, r.fecha
         from users u
         JOIN reserva r on r.id_socio = u.id
         JOIN horarioscancha h on h.id_horario = r.id_horario
-        WHERE id_cancha = $1` ,
-        [idCancha]);
+        WHERE id_cancha = $1 and r.fecha = $2 ` , 
+        [idCancha, fecha]);
         //console.log("RESULT : " + JSON.stringify(result));
         res.json(result.rows);
     } catch (error) {
@@ -68,11 +70,11 @@ const GetCanchaReservaciones = async (req, res) => {
     }
 }
 
-const GetReservaDetails = async (req, res) => {
+const GetReservaOwner = async (req, res) => {
     const idReserva = req.params.id_horario;
     /*console.log(JSON.stringify(id));*/    
     try {
-        const result = await db.query(`SELECT u.id, u.username, h.inicio, h.fin, h.id_horario 
+        const result = await db.query(`SELECT u.id, u.username, u.nombres, u.apellidos, h.inicio, h.fin, h.id_horario, r.id_reserva, r.id_cancha
         from users u
         JOIN reserva r on r.id_socio = u.id
         JOIN horarioscancha h on h.id_horario = r.id_horario
@@ -85,9 +87,26 @@ const GetReservaDetails = async (req, res) => {
     }
 }
 
+const GetReservaDetails = async (req, res) => {
+    const idReserva = req.params.idReserva;
+    /*console.log(JSON.stringify(id));*/    
+    try {
+        const result = await db.query(`select r.id_socio, r.id_reserva, u.username invitados, h.inicio, h.fin, ca.nombre_cancha Cancha from reserva r
+        JOIN canchas ca on ca.id_cancha = r.id_cancha
+        JOIN users u on u.id = r.id_socio or u.id = r.id_invitado_uno or u.id = r.id_invitado_dos
+        JOIN horarioscancha h on h.id_horario = r.id_horario
+        WHERE id_reserva = $1` ,
+        [idReserva]);
+        console.log("RESULT : " + result.rows);
+        res.json(result.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
 
 module.exports = {
     addReservacion, GetCanchaReservaciones, 
     GetReservaDetails, UpdateReservacion, 
-    DeleteReservacion
+    DeleteReservacion, GetReservaOwner
 }
--- Routes/Reservaciones.js
+++ Routes/Reservaciones.js
@@ -1,15 +1,15 @@
 const {Router} = require('express')
 const {addReservacion, GetCanchaReservaciones, GetReservaDetails, 
-    UpdateReservacion, DeleteReservacion
+    UpdateReservacion, DeleteReservacion, GetReservaOwner
 } = require('../Controllers/Reservaciones')
 
 const router = new Router();
 
 router.post('/api/createReservation', addReservacion)
 
-router.get('/api/GetCanchaReservaciones/:idCancha', GetCanchaReservaciones)
-router.get('/api/GetReservaDetails/:id_horario', GetReservaDetails)
-//router.get('/api/getSingleHorario/:idHorario', GetReservacionById)
+router.get('/api/GetCanchaReservaciones/:idCancha/:fecha', GetCanchaReservaciones)
+router.get('/api/GetReservaOwner/:id_horario', GetReservaOwner)
+router.get('/api/GetReservaDetails/:idReserva', GetReservaDetails)
 
 router.put('/api/editHorario/:idHorario', UpdateReservacion)
 
